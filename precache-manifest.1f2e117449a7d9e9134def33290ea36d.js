fa36cf5d71c2144f094e416bf4382d99ad20e51e
G <gudzenko.pr@gmail.com>
2021-01-22 19:47:08 +0300

    Initial dist subtree commit

---
+++ precache-manifest.1f2e117449a7d9e9134def33290ea36d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fbafb10602be19c200f",
+    "url": "/firebase-auth/css/app.b320405c.css"
+  },
+  {
+    "revision": "9bf0607d86a0715620da",
+    "url": "/firebase-auth/css/chunk-1484890a.68f41b41.css"
+  },
+  {
+    "revision": "0c539beab73ebec9e14d",
+    "url": "/firebase-auth/css/chunk-vendors.3e4956bb.css"
+  },
+  {
+    "revision": "e74e7e5a7e9cb3240ec5f125eea95e06",
+    "url": "/firebase-auth/index.html"
+  },
+  {
+    "revision": "4fbafb10602be19c200f",
+    "url": "/firebase-auth/js/app.47bcf467.js"
+  },
+  {
+    "revision": "9bf0607d86a0715620da",
+    "url": "/firebase-auth/js/chunk-1484890a.6045e3b2.js"
+  },
+  {
+    "revision": "bcffbf3df2ee8ad68451",
+    "url": "/firebase-auth/js/chunk-5f4ad635.4a6aef1f.js"
+  },
+  {
+    "revision": "a17d01914da733c128da",
+    "url": "/firebase-auth/js/chunk-c420df12.6ff9c400.js"
+  },
+  {
+    "revision": "0c539beab73ebec9e14d",
+    "url": "/firebase-auth/js/chunk-vendors.9331cfb8.js"
+  },
+  {
+    "revision": "33a49028d5ae10670e0cfb1510188c96",
+    "url": "/firebase-auth/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/firebase-auth/robots.txt"
+  }
+]);
--- src/utils/messages.js
+++
@@ -1,8 +0,0 @@
-export default {
-    logout: 'Вы вышли из системы',
-    login: 'Для начала войдите в систему',
-    'auth/user-not-found': 'Пользователя с таким email не существует',
-    'auth/wrong-password': 'Неверный пароль',
-    'auth/email-already-in-use': 'Пользователь с таким email уже существует',
-    updateUserProfile: 'Данные успешно обновлены'
-}
